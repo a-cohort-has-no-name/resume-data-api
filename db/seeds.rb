44059a0b2bc37524f88dc1bea4fd9a07d9be46fb
Ian <imatthews009@gmail.com>
2017-10-30 19:03:48 -0700

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,25 @@
-x = 1
 30.times do 
+  student = Student.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: "#{Faker::Color.color_name}.#{Faker::Beer.hop.downcase.delete(' ')}@gmail.com",
+    phone_number: Faker::PhoneNumber.phone_number,
+    bio: Faker::Company.bs,
+    photo: Faker::LoremPixel.image
+    )
+
+  student.update(
+    linkedin_url: "linkedin.com/in/#{student.first_name[0].downcase}#{student.last_name.downcase}",
+    twitter_handle: "@#{Faker::Hipster.word}_#{student.first_name.downcase}",
+    personal_website: "#{student.first_name.downcase}#{Faker::Ancient.god.downcase}.com")
+  student.update(
+     resume_url: "#{student.personal_website}/resume",
+     github_url: "github.com/#{student.twitter_handle}")
+  student.save
+end
+
+x = 1
+60.times do 
     Skill.create(
         skill_name: Faker::Job.key_skill,
         student_id: x)  
@@ -16,4 +36,29 @@ y=1
         screenshot: Faker::LoremPixel.image,
         student_id: y)
     y = y + 1
+end
+ 
+
+a = 1
+30.times do
+  Experience.create(
+    start_date: Faker::Date.between(2.years.ago, 30.days.ago),
+    end_date: Faker::Date.between(30.days.ago, Date.today),
+    job_title: Faker::Job.title,
+    company_name: Faker::Company.name,
+    details: Faker::Job.key_skill,
+    student_id: a)
+  a = a + 1
+end
+
+b = 1
+30.times do
+  Education.create(
+    start_date: Faker::Date.between(9.years.ago, 5.years.ago),
+    end_date: Faker::Date.between(5.years.ago, 30.days.ago),
+    degree: Faker::Educator.course,
+    university_name: Faker::Educator.university,
+    details: Faker::Lorem.sentences,
+    student_id: b)
+  b = b + 1
 end
