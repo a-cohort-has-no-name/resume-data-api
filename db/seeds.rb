8c92e47895e5c5636c04557ef047a45a0b7ae09e
Ian <imatthews009@gmail.com>
2017-10-31 19:07:51 -0700

    seed bug fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,15 @@
     email: "#{Faker::Color.color_name}.#{Faker::Beer.hop.downcase.delete(' ')}@gmail.com",
     phone_number: Faker::PhoneNumber.phone_number,
     bio: Faker::Company.bs,
-    photo: Faker::LoremPixel.image)
-    )
+    photo: Faker::LoremPixel.image)  
+
 
   student.update(
     linkedin_url: "linkedin.com/in/#{student.first_name[0].downcase}#{student.last_name.downcase}",
     twitter_handle: "@#{Faker::Hipster.word}_#{student.first_name.downcase}",
     personal_website: "#{student.first_name.downcase}#{Faker::Ancient.god.downcase}.com")
+
+
   student.update(
      resume_url: "#{student.personal_website}/resume",
      github_url: "github.com/#{student.twitter_handle}",
