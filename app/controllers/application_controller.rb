24ee3db217db779fb48ce5c7c6b2f78aa7e82fe2
goodatsports <goodatsports@gmail.com>
2017-10-31 20:47:44 -0700

    Fixed application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def aunthenticate_user!
-    @current_user ||= Student.first(email: params[:email])
+  def authenticate_user!
+    @current_user = Student.find_by(email: params[:email])
     if @current_user && @current_user.password_digest.is_password?(params[:password])
       return true
     else 
